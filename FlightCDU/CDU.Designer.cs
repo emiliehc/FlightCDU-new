0dd1891b603d75e1c16749997f9eb0076a3cd468
nanjingchj <nanjingchj@gmail.com>
2019-06-04 16:51:53 -0400

    finished numpad

--- FlightCDU/CDU.Designer.cs
+++ FlightCDU/CDU.Designer.cs
@@ -78,8 +78,22 @@
             this.btnClear = new System.Windows.Forms.Button();
             this.btnPeriod = new System.Windows.Forms.Button();
             this.pnlKeyboardLetters = new System.Windows.Forms.Panel();
+            this.pnlNumPad = new System.Windows.Forms.Panel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnPlusMinus = new System.Windows.Forms.Button();
+            this.btnComma = new System.Windows.Forms.Button();
             this.pnlScreen.SuspendLayout();
             this.pnlKeyboardLetters.SuspendLayout();
+            this.pnlNumPad.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlScreen
@@ -566,7 +580,8 @@
             // 
             // btnPeriod
             // 
-            this.btnPeriod.Location = new System.Drawing.Point(17, 292);
+            this.btnPeriod.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnPeriod.Location = new System.Drawing.Point(19, 149);
             this.btnPeriod.Name = "btnPeriod";
             this.btnPeriod.Size = new System.Drawing.Size(40, 40);
             this.btnPeriod.TabIndex = 47;
@@ -576,8 +591,8 @@
             // 
             // pnlKeyboardLetters
             // 
+            this.pnlKeyboardLetters.Controls.Add(this.btnComma);
             this.pnlKeyboardLetters.Controls.Add(this.btnA);
-            this.pnlKeyboardLetters.Controls.Add(this.btnPeriod);
             this.pnlKeyboardLetters.Controls.Add(this.btnB);
             this.pnlKeyboardLetters.Controls.Add(this.btnClear);
             this.pnlKeyboardLetters.Controls.Add(this.btnC);
@@ -611,10 +626,151 @@
             this.pnlKeyboardLetters.Controls.Add(this.btnP);
             this.pnlKeyboardLetters.Controls.Add(this.btnN);
             this.pnlKeyboardLetters.Controls.Add(this.btnO);
-            this.pnlKeyboardLetters.Location = new System.Drawing.Point(192, 368);
+            this.pnlKeyboardLetters.Location = new System.Drawing.Point(187, 396);
             this.pnlKeyboardLetters.Name = "pnlKeyboardLetters";
-            this.pnlKeyboardLetters.Size = new System.Drawing.Size(258, 350);
+            this.pnlKeyboardLetters.Size = new System.Drawing.Size(254, 350);
             this.pnlKeyboardLetters.TabIndex = 48;
+            // 
+            // pnlNumPad
+            // 
+            this.pnlNumPad.Controls.Add(this.btn6);
+            this.pnlNumPad.Controls.Add(this.btnPlusMinus);
+            this.pnlNumPad.Controls.Add(this.btnPeriod);
+            this.pnlNumPad.Controls.Add(this.btn0);
+            this.pnlNumPad.Controls.Add(this.btn9);
+            this.pnlNumPad.Controls.Add(this.btn8);
+            this.pnlNumPad.Controls.Add(this.btn7);
+            this.pnlNumPad.Controls.Add(this.btn5);
+            this.pnlNumPad.Controls.Add(this.btn4);
+            this.pnlNumPad.Controls.Add(this.btn3);
+            this.pnlNumPad.Controls.Add(this.btn2);
+            this.pnlNumPad.Controls.Add(this.btn1);
+            this.pnlNumPad.Location = new System.Drawing.Point(20, 539);
+            this.pnlNumPad.Name = "pnlNumPad";
+            this.pnlNumPad.Size = new System.Drawing.Size(161, 207);
+            this.pnlNumPad.TabIndex = 49;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(19, 11);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(40, 40);
+            this.btn1.TabIndex = 48;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(65, 11);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(40, 40);
+            this.btn2.TabIndex = 49;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(111, 11);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(40, 40);
+            this.btn3.TabIndex = 50;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(19, 57);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(40, 40);
+            this.btn4.TabIndex = 51;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(65, 57);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(40, 40);
+            this.btn5.TabIndex = 52;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(111, 57);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(40, 40);
+            this.btn6.TabIndex = 53;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(19, 103);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(40, 40);
+            this.btn7.TabIndex = 54;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(65, 103);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(40, 40);
+            this.btn8.TabIndex = 55;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(111, 103);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(40, 40);
+            this.btn9.TabIndex = 56;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(65, 149);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(40, 40);
+            this.btn0.TabIndex = 57;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btnPlusMinus
+            // 
+            this.btnPlusMinus.Location = new System.Drawing.Point(111, 149);
+            this.btnPlusMinus.Name = "btnPlusMinus";
+            this.btnPlusMinus.Size = new System.Drawing.Size(40, 40);
+            this.btnPlusMinus.TabIndex = 58;
+            this.btnPlusMinus.Text = "-";
+            this.btnPlusMinus.UseVisualStyleBackColor = true;
+            this.btnPlusMinus.Click += new System.EventHandler(this.btnKeyPressed);
+            // 
+            // btnComma
+            // 
+            this.btnComma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnComma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnComma.Location = new System.Drawing.Point(17, 292);
+            this.btnComma.Name = "btnComma";
+            this.btnComma.Size = new System.Drawing.Size(40, 40);
+            this.btnComma.TabIndex = 59;
+            this.btnComma.Text = ",";
+            this.btnComma.UseVisualStyleBackColor = true;
+            this.btnComma.Click += new System.EventHandler(this.btnKeyPressed);
             // 
             // CDU
             // 
@@ -622,6 +778,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(464, 758);
+            this.Controls.Add(this.pnlNumPad);
             this.Controls.Add(this.pnlKeyboardLetters);
             this.Controls.Add(this.btnR6);
             this.Controls.Add(this.btnR5);
@@ -643,6 +800,7 @@
             this.pnlScreen.ResumeLayout(false);
             this.pnlScreen.PerformLayout();
             this.pnlKeyboardLetters.ResumeLayout(false);
+            this.pnlNumPad.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -699,6 +857,19 @@
         private System.Windows.Forms.Button btnPeriod;
         private System.Windows.Forms.Panel pnlKeyboardLetters;
         private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Panel pnlNumPad;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnPlusMinus;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btnComma;
     }
 }
 
