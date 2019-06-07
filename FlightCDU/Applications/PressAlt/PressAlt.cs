8bd693e8b18723d68a17a752fa0b579844040509
nanjingchj <nanjingchj@gmail.com>
2019-06-07 12:46:00 -0400

    more press alt calc labels

--- FlightCDU/Applications/PressAlt/PressAlt.cs
+++ FlightCDU/Applications/PressAlt/PressAlt.cs
@@ -24,6 +24,14 @@ namespace FlightCDU.Applications.PressAlt
             Program.WriteText("L", 1, "▯▯.▯▯");
             Program.WriteLabel("R", 1, "QNH");
             Program.WriteText("R", 1, "▯▯▯▯");
+            // field elevation label
+            Program.WriteLabel("L", 2, "FIELD ELEVATION");
+            Program.WriteText("L", 2, "▯▯▯▯▯");
+            // output label
+            Program.WriteLabel("L", 4, "PRESSURE ALTITUDE");
+            Program.WriteText("L", 4, "- - - - -");
+            // run label
+            Program.WriteText("R", 4, "CALC>");
         }
 
         public static void BtnClicked(string side, int num)
