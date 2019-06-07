0da284b63ac9ffb27e0e5999d6463720261a49f6
nanjingchj <nanjingchj@gmail.com>
2019-06-07 11:38:02 -0400

    Added input labels for qnh and alt setting

--- FlightCDU/Applications/PressAlt/PressAlt.cs
+++ FlightCDU/Applications/PressAlt/PressAlt.cs
@@ -8,6 +8,8 @@ namespace FlightCDU.Applications.PressAlt
 {
     class PressAlt
     {
+        private static double altSetting;
+
         public static void Start()
         {
             Program.CDUClear();
@@ -17,6 +19,11 @@ namespace FlightCDU.Applications.PressAlt
             Program.WritePage("1/1");
             // back to menu
             Program.WriteText("L", 6, "<MENU");
+            // input for the altimeter setting or the qnh
+            Program.WriteLabel("L", 1, "ALTIMETER SETTING");
+            Program.WriteText("L", 1, "▯▯.▯▯");
+            Program.WriteLabel("R", 1, "QNH");
+            Program.WriteText("R", 1, "▯▯▯▯");
         }
 
         public static void BtnClicked(string side, int num)
